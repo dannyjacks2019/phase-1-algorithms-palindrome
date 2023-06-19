6253a8d4e6a615fdc7ee7c50d4b296d462840104
Dan Jackson <dannyjacks2019@gmail.com>
2023-06-19 17:05:00 -0400

    algos palindrome

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const { consoleLog } = require("mocha/lib/reporters/base");
+
 function isPalindrome(word) {
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i
+    if (word[i] !== word[j]) return false
   // Write your algorithm here
+}
+return true
 }
 
 /* 
   Add your pseudocode here
-*/
-
+if the first letter is the same as the last, the 2nd letter is the same as the 2nd to last and the 3rd is the same as the 3rd to last until the middle two letters are reached.. Starting from behinning to the modd;e each letter should from the beginning should correspond with the end. if the letters dont match return false.*/
 /*
   Add written explanation of your solution here
 */
@@ -20,6 +26,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("")
+
+  console.log("expecting: false")
+  console.log('=>', isPalindrome('abba'))
+
 }
 
 module.exports = isPalindrome;
